faf5b7d545217d2d5b372499e4ee92318413a603
Sandra Yeboah <119595994+sandrayeboahAE@users.noreply.github.com>
2023-04-13 15:44:28 +0000

    defined page layout

---
+++ components/Layout.jsx
@@ -0,0 +1,13 @@
+import { Footer, Navbar } from "@mantine/core";
+
+const Layout = ({ children }) => {
+  return (
+    <div>
+      <Navbar />
+      {children}
+      <Footer />
+    </div>
+  );
+};
+
+export default Layout;
